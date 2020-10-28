ecf8ef96553f217a5c50d6abaa364dc023a6b245
sobanjawaid26 <sobanjawaid26@gmail.com>
2020-10-28 21:46:05 +0530

    first commit

---
+++ BST_Classes.js
@@ -0,0 +1,22 @@
+class Node {
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor(){
+        this.root = null;
+    }
+}
+
+var tree = new BinarySearchTree();
+
+console.log(tree)
+
+tree.root = new Node(10);
+tree.root.right = new Node(15);
+tree.root.left = new Node(7);
+tree.root.left.right = new Node(9);
